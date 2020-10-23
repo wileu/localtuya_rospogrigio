1a63093e8f2411317b90c1aeeaa598bcd4db7973
rospogrigio <rospogrigio@gmail.com>
2020-10-23 20:49:52 +0200

    fixed syntax

--- custom_components/localtuya/vacuum.py
+++ custom_components/localtuya/vacuum.py
@@ -66,46 +66,51 @@ class LocaltuyaVacuum(LocalTuyaEntity, VacuumEntity):
     @property
     def cleaning_mode_list(self):
         """Return the list of available fan speeds and cleaning modes."""
+#        return {"smart", "spirale", "random"}
         return NotImplementedError()
 
-    async def async_turn_on():
+    async def async_turn_on(self, **kwargs):
         """Turn the vacuum on and start cleaning."""
         return None
 
-    async def async_turn_off():
+    async def async_turn_off(self, **kwargs):
         """Turn the vacuum off stopping the cleaning and returning home."""
         return None
 
-    async def async_return_to_base():
+    async def async_return_to_base(self, **kwargs):
         """Set the vacuum cleaner to return to the dock."""
         return None
 
-    async def async_stop():
+    async def async_stop(self, **kwargs):
         """Stop the vacuum cleaner, do not return to base."""
         return None
 
-    async def async_clean_spot():
+    async def async_clean_spot(self, **kwargs):
         """Perform a spot clean-up."""
         return None
 
-    async def async_locate():
+    async def async_locate(self, **kwargs):
         """Locate the vacuum cleaner."""
         return None
 
-    async def async_set_cleaning_mode():
+    async def async_set_cleaning_mode(self, **kwargs):
         """Set the cleaning mode."""
+        _LOGGER.debug("Setting cleaning mode: %r", kwargs)
         return None
 
-    async def async_send_command():
+    async def async_send_command(self, **kwargs):
         """Send a command to a vacuum cleaner."""
+        _LOGGER.debug("Sending command: %r", kwargs)
         return None
 
     def status_updated(self):
         """Device status was updated."""
+        print("STATUS UPDATE")
         self._state = self.dps(self._dps_id)
 
         if self.has_config(CONF_BATTERY_DP):
             self._battery_level = self.dps_conf(CONF_BATTERY_DP)
+        print("STATUS UPDATED [{}] [{}]".format(self._state, self._battery_level))
 
 
 async_setup_entry = partial(async_setup_entry, DOMAIN, LocaltuyaVacuum, flow_schema)
