jfparis/localtuya

Add DP suggestions to switch platforms

custom_components/localtuya/light.py
@@ -15,7 +15,7 @@ from homeassistant.components.light import (
     SUPPORT_COLOR_TEMP,
     LightEntity,
 )
-from homeassistant.const import CONF_BRIGHTNESS, CONF_COLOR_TEMP
+from homeassistant.const import CONF_ID, CONF_BRIGHTNESS, CONF_COLOR_TEMP
 
 from .common import LocalTuyaEntity, async_setup_entry
 from .const import (
@@ -28,6 +28,14 @@ from .const import (
 )
 
 _LOGGER = logging.getLogger(__name__)
+
+DP_SUGGESTIONS = {
+    CONF_ID: [1, 20],
+    CONF_COLOR_MODE: [2, 21],
+    CONF_BRIGHTNESS: [3, 22],
+    CONF_COLOR_TEMP: [4, 23],
+    CONF_COLOR: [5, 24],
+}
 
 MIRED_TO_KELVIN_CONST = 1000000
 DEFAULT_MIN_KELVIN = 2700  # MIRED 370

wileu/localtuya_rospogrigio

custom_components/localtuya/light.py
@@ -15,7 +15,7 @@ from homeassistant.components.light import (
     SUPPORT_COLOR_TEMP,
     LightEntity,
 )
-from homeassistant.const import CONF_BRIGHTNESS, CONF_COLOR_TEMP
+from homeassistant.const import CONF_BRIGHTNESS, CONF_COLOR_TEMP, CONF_ID
 
 from .common import LocalTuyaEntity, async_setup_entry
 from .const import (
@@ -28,6 +28,14 @@ from .const import (
 )
 
 _LOGGER = logging.getLogger(__name__)
+
+DP_SUGGESTIONS = {
+    CONF_ID: [1, 20],
+    CONF_COLOR_MODE: [2, 21],
+    CONF_BRIGHTNESS: [3, 22],
+    CONF_COLOR_TEMP: [4, 23],
+    CONF_COLOR: [5, 24],
+}
 
 MIRED_TO_KELVIN_CONST = 1000000
 DEFAULT_MIN_KELVIN = 2700  # MIRED 370